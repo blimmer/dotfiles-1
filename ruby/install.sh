6385b17f56d9fd1a83c4255dad75c66472662eaf
Ben Limmer <ben@benlimmer.com>
2014-06-08 15:17:38 -0600

    Slight mods.

--- ruby/install.sh
+++ ruby/install.sh
@@ -11,3 +11,9 @@ then
   echo "  Installing ruby-build for you."
   brew install ruby-build > /tmp/ruby-build-install.log
 fi
+
+if test ! $(which readline)
+then
+  echo "  Installing readline for you."
+  brew install readline > /tmp/readline-install.log
+fi
